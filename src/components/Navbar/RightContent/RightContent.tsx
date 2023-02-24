436d877d6d38d593662dcb2a0be6964358f28fa0
Yuliia <julia.novak9@gmail@com>
2023-02-24 18:16:55 +0100

    add Logout btn

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,19 +1,26 @@
-import AuthModal from '@/components/Modal/Auth/AuthModal';
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
-import AuthButtons from './AuthButtons';
+import AuthModal from "@/components/Modal/Auth/AuthModal";
+import { auth } from "@/firebase/clientApp";
+import { Flex, Button } from "@chakra-ui/react";
+import { signOut } from "firebase/auth";
+import React from "react";
+import AuthButtons from "./AuthButtons";
 
 type RightContentProps = {
-  // user:
+   user: any;
 };
 
-const RightContent:React.FC<RightContentProps> = () => {
-  
-  return <>
-    <AuthModal/>
-    <Flex justify='center' align='center'>
-      <AuthButtons />
-    </Flex>
-  </>
-}
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
+   return (
+      <>
+         <AuthModal />
+         <Flex justify="center" align="center">
+            {user ? (
+               <Button onClick={() => signOut(auth)}>Logout</Button>
+            ) : (
+               <AuthButtons />
+            )}
+         </Flex>
+      </>
+   );
+};
 export default RightContent;
