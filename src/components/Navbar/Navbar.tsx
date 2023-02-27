b4ab17a8b3c1d7c4c4f8673f3ee8645a81b543cd
Yuliia <julia.novak9@gmail@com>
2023-02-27 14:32:13 +0100

    add communities dropdown

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,42 @@
-import { auth } from '@/firebase/clientApp';
-import { Flex, Image } from '@chakra-ui/react';
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import RightContent from './RightContent/RightContent';
-import SearchInput from './SearchInput';
+import { auth } from "@/firebase/clientApp";
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Directory from "./Directory/Directory";
+import RightContent from "./RightContent/RightContent";
+import SearchInput from "./SearchInput";
 
-const Navbar:React.FC = () => {
-  const [user, loading, error] = useAuthState(auth)
+const Navbar: React.FC = () => {
+   const [user, loading, error] = useAuthState(auth);
 
-  return (
-    <Flex bg='white' height='44px' padding='6px 12px'>
-      <Flex align='center'>
-        <Image src='./images/redditFace.svg' alt='reddit logo' height='30px'/>
-        <Image src='./images/redditText.svg' alt='reddit logo'height='46px' display={{base: 'none', md: 'unset'}}/>
+   return (
+      <Flex
+         bg="white"
+         height="44px"
+         padding="6px 12px"
+         justify={{ md: "space-between" }}
+      >
+         <Flex
+            align="center"
+            width={{ base: "40px", md: "auto" }}
+            mr={{ base: 0, md: 2 }}
+         >
+            <Image
+               src="./images/redditFace.svg"
+               alt="reddit logo"
+               height="30px"
+            />
+            <Image
+               src="./images/redditText.svg"
+               alt="reddit logo"
+               height="46px"
+               display={{ base: "none", md: "unset" }}
+            />
+         </Flex>
+         {user && <Directory />}
+         <SearchInput user={user} />
+         <RightContent user={user} />
       </Flex>
-      {/* <Directory /> */}
-      <SearchInput />
-      <RightContent user={user}/>
-    </Flex>
-  )
-}
+   );
+};
 export default Navbar;
